9bf5d25f177ce9eeb896bf05fa8b05435dd52e4d
Dhruti Shah <dhrutishah10@gmail.com>
2015-02-27 17:08:19 +0300

    added more code for ldrawer

--- app/src/main/java/comd7shah/github/www/enrique/Main.java
+++ app/src/main/java/comd7shah/github/www/enrique/Main.java
@@ -1,25 +1,33 @@
 package comd7shah.github.www.enrique;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.ikimuhendis.ldrawer.ActionBarDrawerToggle;
 import com.ikimuhendis.ldrawer.DrawerArrowDrawable;
-
-import static comd7shah.github.www.enrique.R.color.*;
 
 
 public class Main extends Activity {
     private DrawerArrowDrawable drawerArrow;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private boolean drawerArrowColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navdrawer);
 
         drawerArrow = new DrawerArrowDrawable(this) {
             @Override
@@ -43,15 +51,87 @@ public class Main extends Activity {
             }
         };
 
-        mDrawerToggle.setAnimateEnabled(false);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
 
-        drawerArrow.setProgress(0f); // normal position
-        drawerArrow.setProgress(1f); // back arrow position
-        drawerArrow.setColor(ldrawer_color); // to set color
+
+        String[] values = new String[]{
+                "Stop Animation (Back icon)",
+                "Stop Animation (Home icon)",
+                "Start Animation",
+                "Change Color",
+                "GitHub Page",
+                "Share",
+                "Rate"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        mDrawerToggle.setAnimateEnabled(false);
+                        drawerArrow.setProgress(1f);
+                        break;
+                    case 1:
+                        mDrawerToggle.setAnimateEnabled(false);
+                        drawerArrow.setProgress(0f);
+                        break;
+                    case 2:
+                        mDrawerToggle.setAnimateEnabled(true);
+                        mDrawerToggle.syncState();
+                        break;
+                    case 3:
+                        if (drawerArrowColor) {
+                            drawerArrowColor = false;
+                            drawerArrow.setColor(R.color.ldrawer_color);
+                        } else {
+                            drawerArrowColor = true;
+                            drawerArrow.setColor(R.color.drawer_arrow_second_color);
+                        }
+                        mDrawerToggle.syncState();
+                        break;
+                    case 4:
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/IkiMuhendis/LDrawer"));
+                        startActivity(browserIntent);
+                        break;
+                    case 5:
+                        Intent share = new Intent(Intent.ACTION_SEND);
+                        share.setType("text/plain");
+                        share.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        share.putExtra(Intent.EXTRA_SUBJECT,
+                                getString(R.string.app_name));
+                        share.putExtra(Intent.EXTRA_TEXT, getString(R.string.app_description) + "\n" +
+                                "GitHub Page :  https://github.com/IkiMuhendis/LDrawer\n" +
+                                "Sample App : https://play.google.com/store/apps/details?id=" +
+                                getPackageName());
+                        startActivity(Intent.createChooser(share,
+                                getString(R.string.app_name)));
+                        break;
+                    case 6:
+                        String appUrl = "https://play.google.com/store/apps/details?id=" + getPackageName();
+                        Intent rateIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(appUrl));
+                        startActivity(rateIntent);
+                        break;
+                }
+
+            }
+        });
     }
 
 
-
-
-
 }
+
+
+
+
+
+
+
+
+
